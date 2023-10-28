498a602220fc1c183a8e4f1faa5484bf14abe46f
ConcordDK <148088072+ConcordDK@users.noreply.github.com>
2023-10-28 15:32:52 +0400

    2

--- src/main/java/AreaSquare.java
+++ src/main/java/AreaSquare.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class AreaSquare {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите одну из сторон квадрата:");
 
+        System.out.println("Введите одну из сторон квадрата:");
         double side = scanner.nextDouble();
 
-        double area = side * 2;
+        double area = Math.pow(side, 2);
         System.out.println("Площадь квадрата равна: " + area);
     }
 }
